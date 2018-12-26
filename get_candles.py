a05d44891cb288f497c42726bc10fac5183609d1
kobayashitakaki <takaki@takakisan.com>
2018-12-27 01:52:16 +0900

    calculate macd

--- get_candles.py
+++ get_candles.py
@@ -27,7 +27,7 @@ kwargs = {}
 kwargs['granularity'] = 'M5'
 #kwargs['fromTime'] = from_time
 kwargs['toTime'] = to_time
-kwargs['count'] = 31
+kwargs['count'] = 60
 
 api = v20.Context(
         hostname,
@@ -72,7 +72,17 @@ mapped_list = map(lambda candle: format_candle(candle), candles)
 #print(list(mapped_list))
 
 df = pd.DataFrame(list(mapped_list))
-print(df.loc[:,['datetime','open','high','low','close']])
+
+
+macd = pd.DataFrame()
+macd['ema12'] = df['close'].ewm(span=12).mean()
+macd['ema26'] = df['close'].ewm(span=26).mean()
+macd['macd'] = macd['ema12'] - macd['ema26']
+macd['signal'] = macd['macd'].rolling(window=9).mean()
+df['macd'] = macd['macd']
+df['macd_signal'] = macd['signal']
+df['macd2'] = macd['macd'] - macd['signal']
+print(df.loc[:,['datetime','open','high','low','close', 'macd', 'macd_signal', 'macd2']])
 
 
 #df = json_normalize(json.loads(candles))
