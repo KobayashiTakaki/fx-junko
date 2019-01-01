4117deb0f699e3833f61b2e1347f38a0c605a057
kobayashitakaki <takaki@takakisan.com>
2019-01-01 22:01:04 +0900

    modifiy scheduler for week-days

--- scheduler.py
+++ scheduler.py
@@ -9,15 +9,28 @@ trader = trader.Trader()
 def trader_loop():
     trader.loop()
 
-
 def analyzer_loop():
     analyzer.loop()
+
+def activate():
+    schedule.clear(tag='fx')
+    schedule.every(60).seconds.do(trader_loop).tag('fx')
+    schedule.every(60).seconds.do(analyzer_loop).tag('fx')
+
+def deactivate():
+    trader.exit()
+    schedule.clear(tag='fx')
 
 def delete_old_log():
     db.delete_old_log()
 
-schedule.every(60).seconds.do(trader_loop)
-schedule.every(120).seconds.do(analyzer_loop)
+schedule.every().monday.at('08:00').do(activate)
+schedule.every().tuesday.at('08:00').do(activate)
+schedule.every().wednesday.at('08:00').do(activate)
+schedule.every().thursday.at('08:00').do(activate)
+schedule.every().friday.at('08:00').do(activate)
+schedule.every().saturday.at('05:00').do(deactivate)
+
 schedule.every(1).weeks.do(delete_old_log)
 
 while True:
