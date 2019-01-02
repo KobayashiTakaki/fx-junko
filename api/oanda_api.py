00ccaf7d5a3e683326d3783a343720c29a32bc87
kobayashitakaki <takaki@takakisan.com>
2019-01-02 19:09:19 +0900

    modify format_trade

--- api/oanda_api.py
+++ api/oanda_api.py
@@ -34,17 +34,33 @@ def format_candle(candle):
     }
 
 def format_trade(trade):
+    tradeId = trade.id
+    instrument = trade.instrument
+    price = trade.price
+    openTime = str(datetime.datetime.strptime(
+        trade.openTime.split('.')[0] + '+00:00',
+        '%Y-%m-%dT%H:%M:%S%z'))
+    state = trade.state
+    initialUnits = trade.initialUnits
+    realizedPL = '' if trade.realizedPL is None else trade.realizedPL
+    unrealizedPL = '' if trade.unrealizedPL is None else trade.unrealizedPL
+    averageClosePrice = '' if trade.averageClosePrice is None else trade.averageClosePrice
+    closeTime = '' if trade.closeTime is None \
+        else str(datetime.datetime.strptime(
+            trade.closeTime.split('.')[0] + '+00:00',
+            '%Y-%m-%dT%H:%M:%S%z'))
+
     return {
-        'tradeId': trade.id,
-        'instrument': trade.instrument,
-        'price': trade.price,
-        'openTime': trade.openTime,
-        'state': trade.state,
-        'initialUnits': trade.initialUnits,
-        'realizedPL': trade.realizedPL,
-        'unrealizedPL': trade.unrealizedPL,
-        'averageClosePrice': trade.averageClosePrice,
-        'closeTime': trade.closeTime
+        'tradeId': tradeId,
+        'instrument': instrument,
+        'price': price,
+        'openTime': openTime,
+        'state': state,
+        'initialUnits': initialUnits,
+        'realizedPL': realizedPL,
+        'unrealizedPL': unrealizedPL,
+        'averageClosePrice': averageClosePrice,
+        'closeTime': closeTime
     }
 
 def get_candles(instrument=instrument, params=candles_params, completed_only=True):
