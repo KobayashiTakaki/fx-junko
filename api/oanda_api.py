f5a948308d432b343b1a842f294de55de549e272
kobayashitakaki <takaki@takakisan.com>
2019-01-03 23:04:50 +0900

    change stop_loss distance

--- api/oanda_api.py
+++ api/oanda_api.py
@@ -74,7 +74,7 @@ def get_candles(instrument=instrument, params=candles_params, completed_only=Tru
 
 def market_order(units):
     stop_loss = {
-        'distance': str(0.100)
+        'distance': str(0.180)
     }
 
     params = {
