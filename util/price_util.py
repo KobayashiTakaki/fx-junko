d54de1c0512230abf3f72c68cbb06f92892b374f
kobayashitakaki <takaki@takakisan.com>
2019-01-11 23:11:44 +0900

    cutout calc_macd to util

---
+++ util/price_util.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def calc_macd(df):
+    macd = pd.DataFrame()
+    macd['ema12'] = df['close'].ewm(span=12).mean()
+    macd['ema26'] = df['close'].ewm(span=26).mean()
+    macd['macd'] = macd['ema12'] - macd['ema26']
+    macd['signal'] = macd['macd'].rolling(window=9).mean()
+    df['macd'] = macd['macd']
+    df['macd_signal'] = macd['signal']
+    df['macd2'] = macd['macd'] - macd['signal']
+    #macd_direction: macdがシグナルより上なら1, シグナル以下なら-1
+    df['macd_direction'] = df['macd2'].apply(lambda v: 1 if v > 0 else -1)
+
+    #macdのcrossを判定
+    for i in range(1, len(df)):
+        last_price = df.iloc[i-1]
+        price = df.iloc[i]
+        if last_price['macd_direction'] > price['macd_direction']:
+            #1つ前のmacd_drectionが大きい->下向きにクロスした
+            df.at[i, 'crossed'] = str(-1)
+        elif last_price['macd_direction'] < price['macd_direction']:
+            #1つ前のmacd_drectionが小さい->上向きにクロスした
+            df.at[i, 'crossed'] = str(1)
+        else:
+            df.at[i, 'crossed'] = str(0)
+
+    return df
