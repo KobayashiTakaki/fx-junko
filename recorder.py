2e1b1e1490c7559fd4e444410b14c30ffbe8228f
kobayashitakaki <takaki@takakisan.com>
2019-01-11 21:40:44 +0900

    implement update_trade_data in recorder

--- recorder.py
+++ recorder.py
@@ -1,5 +1,7 @@
+import datetime
 import pandas as pd
 import db.db  as db
+import api.oanda_api as oanda_api
 
 conn = db.conn
 db_time_fromat = db.time_format
@@ -31,3 +33,25 @@ def add_trade_record(trade, table_name):
     if len(records) == 0:
         df = pd.DataFrame(trade, index=[1])
         df.to_sql(table_name, conn, if_exists="append", index=False)
+
+def update_trade_data(table_name):
+    #tradesテーブルからOPENのtrade_idを取得
+    open_ids = list(pd.read_sql_query(
+        'select tradeId from ' + table_name + ' '
+        + 'where state=\'OPEN\''
+        ,conn
+    )['tradeId'])
+
+    fetched_trades = pd.DataFrame(columns=trades_header)
+    for id in open_ids:
+        trade = oanda_api.get_trade(id)
+        s = pd.Series(trade)
+        fetched_trades = fetched_trades.append(s,ignore_index=True)
+
+    conn.execute(
+        'delete from '+ table_name + ' where tradeId in ('
+        + ','.join(open_ids) + ');'
+    )
+    conn.commit()
+
+    fetched_trades.to_sql(table_name, conn, if_exists="append", index=False)
