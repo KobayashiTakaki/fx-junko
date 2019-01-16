3eea188bca8c6788fc423d46017cfbe150256f09
kobayashitakaki <takaki@takakisan.com>
2019-01-16 22:19:32 +0900

    change is_scalping_suitable time scope

--- analyzer.py
+++ analyzer.py
@@ -196,7 +196,7 @@ def market_trend():
 def is_scalping_suitable():
     table_name = 'prices_1min'
     time_from = (datetime.datetime.now(datetime.timezone.utc)\
-        - datetime.timedelta(minutes=30)).strftime(db_time_format)
+        - datetime.timedelta(minutes=10)).strftime(db_time_format)
     df = pd.read_sql_query(
         'select * from ' + table_name + ' '
         + 'where datetmie > \'' + time_from + '\';'
