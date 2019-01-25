5605d7884ebabb23b342f6d68a45194c2a24c8ab
kobayashitakaki <o.share.thief@gmail.com>
2019-01-25 20:38:49 +0900

    fix is_candle_over_middle args

--- util/trader_util.py
+++ util/trader_util.py
@@ -67,7 +67,7 @@ def is_candle_over_bollinger(time_unit='M', time_count=5):
 
     return False, 0
 
-def is_candle_over_middle(time_unit='M', time_count=5, toward):
+def is_candle_over_middle(time_unit='M', time_count=5, toward='up'):
     table_name = 'prices_{0}{1}'.format(time_unit, time_count)
 
     # 最新のレコードを1件取得
