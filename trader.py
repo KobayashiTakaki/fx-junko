5e04d3454500627ea5f1a46dc0173bda3b4398a8
kobayashitakaki <o.share.thief@gmail.com>
2019-01-25 23:48:33 +0900

    change trader logging

--- trader.py
+++ trader.py
@@ -5,6 +5,7 @@ import api.twitter_api as twitter_api
 import util.trader_util as util
 import db.db as db
 import recorder
+import logger
 
 class Trader():
     def __init__(self):
@@ -14,36 +15,37 @@ class Trader():
         self.instrument = 'USD_JPY'
         self.time_unit = 'M'
         self.time_count = 5
+        self.logger = logger.get_logger('trader')
 
     def loop(self):
         self.open_trade = oanda_api.get_open_trade()
 
         if self.open_trade is not None:
-            db.write_log('trader', 'i have an open trade')
+            self.logger.debug('i have an open trade')
 
             if int(self.open_trade['initialUnits']) > 0:
                 # 中値を下回った
                 if util.is_candle_over_middle(
                     time_unit=self.time_unit, time_count=self.time_count,
                     toward='down'):
-                    db.write_log('trader', 'over middle toward down. exit.')
+                    self.logger.debug('over middle toward down. exit.')
                     self.exit()
             else:
                 # 中値を上回った
                 if util.is_candle_over_middle(
                     time_unit=self.time_unit, time_count=self.time_count,
                     toward='up'):
-                    db.write_log('trader', 'over middle toward up. exit.')
+                    self.logger.debug('over middle toward up. exit.')
                     self.exit()
 
         else:
             #ポジションがない場合
-            db.write_log('trader', 'i dont have an open trade')
+            self.logger.debug('i dont have an open trade')
             # bollinger bandの上を超えた
             if util.is_candle_over_bollinger(
                 time_unit=self.time_unit, time_count=self.time_count,
                 within=1, toward='up'):
-                db.write_log('trader', 'over upper bollinger')
+                self.logger.debug('over upper bollinger')
 
                 is_macd_crossed = util.is_macd_crossed(
                     time_unit=self.time_unit, time_count=self.time_count,
@@ -55,19 +57,19 @@ class Trader():
                     if not util.is_candle_over_bollinger(
                         time_unit=self.time_unit, time_count=self.time_count,
                         within=15, toward='down'):
-                        db.write_log('trader', 'entry by buy')
+                        self.logger.debug('entry by buy')
                         self.entry('buy')
                         return
                     else:
-                        db.write_log('trader', 'recently overed against bollinger')
+                        self.logger.debug('recently overed against bollinger')
                 else:
-                    db.write_log('trader', 'macd not crossed recently')
+                    self.logger.debug('macd not crossed recently')
 
             # bollinger bandの下を超えた
             elif util.is_candle_over_bollinger(
                 time_unit=self.time_unit, time_count=self.time_count,
                 within=1, toward='down'):
-                db.write_log('trader', 'over lower bollinger')
+                self.logger.debug('over lower bollinger')
 
                 is_macd_crossed = util.is_macd_crossed(
                     time_unit=self.time_unit, time_count=self.time_count,
@@ -79,13 +81,13 @@ class Trader():
                     if not util.is_candle_over_bollinger(
                         time_unit=self.time_unit, time_count=self.time_count,
                         within=15, toward='up'):
-                        db.write_log('trader', 'entry by sell')
+                        self.logger.debug('entry by sell')
                         self.entry('sell')
                         return
                     else:
-                        db.write_log('trader', 'recently overed against bollinger')
+                        self.logger.debug('recently overed against bollinger')
                 else:
-                    db.write_log('trader', 'macd not crossed recently')
+                    self.logger.debug('macd not crossed recently')
 
     def entry(self, side):
         amount = self.entry_amount
@@ -113,11 +115,11 @@ class Trader():
             retry += 1
 
         recorder.add_trade_record(self.open_trade, 'trades')
-        db.write_log('trader', 'open_trade: ' + str(self.open_trade))
+        self.logger.debug('open_trade: ' + str(self.open_trade))
 
     def exit(self):
         if self.open_trade is not None:
-            db.write_log('trader', 'close position')
+            self.logger.debug('close position')
             oanda_api.close_trade(self.open_trade['tradeId'])
             self.open_trade = oanda_api.get_open_trade()
 
