933c0dbc1c3a2881c5af88a9a92ff49544f56f12
kobayashitakaki <o.share.thief@gmail.com>
2019-01-25 23:57:19 +0900

    change twitter api logging

--- api/twitter_api.py
+++ api/twitter_api.py
@@ -2,6 +2,7 @@ import configparser
 from requests_oauthlib import OAuth1Session
 import db.db as db
 import time
+import logger
 
 config = configparser.ConfigParser()
 config.read('api/twitter_conf.ini')
@@ -9,6 +10,7 @@ CK = config['DEFAULT']['CONSUMER_KEY']
 CS = config['DEFAULT']['CONSUMER_SECRET']
 AT = config['DEFAULT']['ACCESS_TOKEN']
 ATS = config['DEFAULT']['ACCESS_TOKEN_SECRET']
+logger = logger.get_logger('tweeter')
 
 def tweet(content):
     session = OAuth1Session(CK, CS, AT, ATS)
@@ -16,7 +18,7 @@ def tweet(content):
     params = {
         'status': content
     }
-    db.write_log('twitter_api', 'content: ' + content)
+    logger.debug('content: ' + content)
 
     done = False
     retry = 0
@@ -27,12 +29,15 @@ def tweet(content):
             if response.status_code != 200:
                 raise Exception('tweet failed')
             else:
-                db.write_log('twitter_api', 'tweet scceeded')
+                logger.debug('tweet scceeded')
                 done = True
         except Exception as e:
-            db.write_log('exception', str(e))
+            logger.debug(str(e))
             #高速連投を避けるためのsleep
             time.sleep(5)
             continue
         finally:
             retry +=1
+
+    if not done:
+        raise Exception('tweet failed')
