c8df3a5899388c9add4bb8c796d06b20a07483f3
kobayashitakaki <o.share.thief@gmail.com>
2019-05-22 21:20:53 +0900

    change trades

--- tests/trades/test_trades.py
+++ tests/trades/test_trades.py
@@ -1,12 +1,15 @@
 import unittest
 from unittest.mock import MagicMock
 from unittest.mock import patch
-from trades import trades
+import trades.trades
+import pandas as pd
+import db.table_defs
 
 class TestTraderTrades(unittest.TestCase):
     @patch('oanda.client.Client')
-    def setUp(self, mock_client):
-        self.trades = trades.Trades()
+    @patch('trades.trades.DbClient')
+    def setUp(self, mock_oanda_client, mock_db_client):
+        self.trades = trades.trades.Trades()
 
     def test_get_open_trades(self):
         open_trade = self.trades.get_open_trade()
@@ -15,16 +18,36 @@ class TestTraderTrades(unittest.TestCase):
     def test_open(self):
         self.trades.update_open_trade = MagicMock()
         self.trades.open(1)
-        self.trades.client.market_order.assert_called_with(1)
+        self.trades.oanda_client.market_order.assert_called_with(1)
         self.trades.update_open_trade.assert_called()
 
     def test_close(self):
         self.trades.update_open_trade = MagicMock()
         self.trades.close(1)
-        self.trades.client.close_trade.assert_called_with(1)
+        self.trades.oanda_client.close_trade.assert_called_with(1)
         self.trades.update_open_trade.assert_called()
 
     def test_update_open_trade(self):
         self.trades.update_open_trade()
-        self.trades.client.get_open_trade.assert_called()
-        self.assertEqual(self.trades.open_trade, self.trades.client.get_open_trade())
+        self.trades.oanda_client.get_open_trade.assert_called()
+        self.assertEqual(self.trades.open_trade, self.trades.oanda_client.get_open_trade())
+
+    def test_update_trade_records(self):
+        # recordsが空の場合
+        records = pd.DataFrame()
+        self.trades.db_client.get_open_trade_records.return_value = records
+        self.trades.update_trade_records()
+        self.trades.db_client.delete_trade_records.assert_not_called()
+        self.trades.oanda_client.get_trade.assert_not_called()
+        self.trades.db_client.insert_trade_records.assert_not_called()
+
+        # recordsが複数ある場合
+        trades = [{'tradeId': 1},{'tradeId': 2}]
+        records = pd.DataFrame(trades)
+        self.trades.db_client.get_open_trade_records.return_value = records
+        self.trades.oanda_client.get_trade.side_effect = trades
+        self.trades.update_trade_records()
+        self.trades.db_client.delete_trade_records.assert_called_with([1, 2])
+        self.trades.oanda_client.get_trade.assert_any_call(1)
+        self.trades.oanda_client.get_trade.assert_any_call(2)
+        self.trades.db_client.insert_trade_records.assert_called()
--- trades/trades.py
+++ trades/trades.py
@@ -1,8 +1,13 @@
-from oanda import client
+import datetime
+import db.sqlite
+import db.table_defs
+import oanda.client
+import pandas as pd
 
 class Trades():
     def __init__(self):
-        self.client = client.Client()
+        self.oanda_client = oanda.client.Client()
+        self.db_client = DbClient()
         self.open_trade = None
 
     def get_open_trade(self):
@@ -10,28 +15,90 @@ class Trades():
 
     def open(self, units):
         try:
-            self.client.market_order(units)
+            self.oanda_client.market_order(units)
         except Exception as e:
-            self.logger.debug(traceback.format_exc())
-            raise TradesError(e)
+            raise TradesError('opening trade failed')
         finally:
             self.update_open_trade()
+            self.db_client.add_trade_record(self.open_trade)
 
     def close(self, id):
         try:
-            self.client.close_trade(id)
+            self.oanda_client.close_trade(id)
         except Exception as e:
-            self.logger.debug(traceback.format_exc())
-            raise TradesError(e)
+            raise TradesError('closing trade failed')
         finally:
             self.update_open_trade()
 
     def update_open_trade(self):
         try:
-            self.open_trade = self.client.get_open_trade()
+            self.open_trade = self.oanda_client.get_open_trade()
         except Exception as e:
-            self.logger.debug(traceback.format_exc())
-            raise TradesError(e)
+            raise TradesError('getting open trade failed')
+
+    def update_trade_records(self):
+        open_trade_records = self.db_client.get_open_trade_records()
+        # 1件も無ければreturn
+        if len(open_trade_records) < 1:
+            return
+
+        ids = list(open_trade_records['tradeId'])
+        self.db_client.delete_trade_records(ids)
+
+        # APIからopen_idのtradeを取得し、DataFrameに追加していく
+        header = db.table_defs.get_columns('trades')
+        fetched_trades = pd.DataFrame(columns=header)
+        for id in ids:
+            try:
+                trade = self.oanda_client.get_trade(id)
+            except Exception as e:
+                self.logger.debug(traceback.format_exc())
+                raise TradesError(e)
+            s = pd.Series(trade)
+            fetched_trades = fetched_trades.append(s, ignore_index=True)
+
+        self.db_client.insert_trade_records(fetched_trades)
+
+class DbClient():
+    def __init__(self):
+        self.conn = db.sqlite.Sqlite().get_conn()
+
+    def create_trades_table(self, table_name='trades'):
+        sql = db.table_defs.get_create_table_sql('trades', table_name)
+        self.conn.execute(sql)
+
+    def add_trade_record(self, trade, table_name='trades'):
+        self.create_trades_table(table_name)
+
+        # 同じidのレコードがあればreturn
+        records = conn.execute('select * from ' + table_name + ' '
+            + 'where tradeId = ' + trade['tradeId'] + ';').fetchall()
+        if len(records) != 0:
+            return
+
+        df = pd.DataFrame(trade, index=[1])
+        df.to_sql(table_name, self.conn, if_exists="append", index=False)
+
+    def get_open_trade_records(self, table_name='trades'):
+        self.create_trades_table(table_name)
+        # tradesテーブルからOPENのtrade_idを取得
+        open_trade_records = pd.read_sql_query(
+            'select tradeId from ' + table_name + ' '
+            + 'where state=\'OPEN\''
+            ,self.conn
+        )
+        return open_trade_records
+
+    def delete_trade_records(self, ids, table_name='trades'):
+        ids = list(map(str, ids))
+        self.conn.execute(
+            'delete from '+ table_name + ' where tradeId in ('
+            + ','.join(ids) + ');'
+        )
+        self.conn.commit()
+
+    def insert_trade_records(self, df, table_name='trades'):
+        df.to_sql(table_name, self.conn, if_exists="append", index=False)
 
 class TradesError(Exception):
     pass
